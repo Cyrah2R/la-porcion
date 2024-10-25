1671d9c8dfd96a867e2ee3b1bd07f1d0ce232d6b
Cyrah2R <cyrahrr@gmail.com>
2024-10-25 16:57:25 +0200

    Actualiza el archivo pizzas.jsx

--- src/components/data/carta/pizzas.jsx
+++ src/components/data/carta/pizzas.jsx
@@ -1,6 +1,7 @@
 export const Pizzas = [
     { id: 1, name: 'Pizza Margherita', description: 'Tomate, mozzarella, albahaca', price: 8.50 },
     { id: 2, name: 'Pizza Pepperoni', description: 'Tomate, mozzarella, pepperoni', price: 9.00 },
+    { id: 2, name: 'Pizza Chapitun', description: 'Tomate, mozzarella, atun', price: 9.00 },
     // Agrega más pizzas aquí
   ];
   
