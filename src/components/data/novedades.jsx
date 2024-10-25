c163d9e51c586c0ba39434667419d29c88d08236
Cyrah Code <135345584+Cyrah2R@users.noreply.github.com>
2024-10-25 16:41:24 +0200

    Add files via upload

---
+++ src/components/data/novedades.jsx
@@ -0,0 +1,16 @@
+export const novedades = [
+  {
+    id: 'novedad1',
+    title: 'Primera Novedad',
+    description: 'Descripción de la primera novedad.',
+    image: require('../assets/images/novedad1.png') // Asegúrate de que la ruta sea correcta
+  },
+  {
+      id: 'novedad2',
+      title: 'Segunda Novedad',
+      description: 'Descripción de la segunda novedad.',
+      image: require('../assets/images/novedad2.png') // Asegúrate de que la ruta sea correcta
+  },
+    // Más novedades...
+  ];
+  
