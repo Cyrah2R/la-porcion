c9ce85389be206002210b02c6397eb0f08a1a9b3
Cyrah2R <cyrahrr@gmail.com>
2024-10-28 14:15:21 +0100

    archivo firebase.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './firebase'; // Importa la configuración de Firebase
 
 const container = document.getElementById("root");
 const root = createRoot(container);
